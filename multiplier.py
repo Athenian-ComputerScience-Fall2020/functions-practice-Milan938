c201c8814c876c3cac1caae85764332dbc355b90
Milan938 <23mboga@athenian.org>
2020-09-15 20:14:17 +0000

    Finished

--- multiplier.py
+++ multiplier.py
@@ -4,3 +4,11 @@ The user should be able to input two numbers that are stored in variables.
 The function should multiply the two variables together and return the result to a variable in the main program.
 The main program should output the variable containing the result returned from the function.
 '''
+def multi():
+    x = int(input("Please enter a number: "))
+    y = int(input("Please enter another number: "))
+    return x * y
+
+output_num = multi()
+
+print(output_num)
