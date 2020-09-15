79c53ddeabe14afdd67f21987c20ee1d93c09301
Milan938 <23mboga@athenian.org>
2020-09-15 20:03:04 +0000

    Finished

--- call_functions_edit.py
+++ call_functions_edit.py
@@ -1,6 +1,8 @@
 # Rearrange the function calls so they make logical sense.
 # Next, define a new function that tells the user a joke (you decide on the function name and the joke). Call it in a sensible place in the program. 
 # Remember that function definitions go at the top of the program, so place your function in an appropriate location.
+def banger_joke():
+    print("When I see the earth rotating, it makes my day.")
 
 def say_hi():
    print("Why hello there!")
@@ -14,7 +16,8 @@ def offer_food():
 def say_bye():
    print("Cheerio then.")
 
-
-offer_drink()
 say_hi()
+banger_joke()
+offer_drink()
 offer_food()
+say_bye()
